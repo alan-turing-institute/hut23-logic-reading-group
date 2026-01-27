17a7fb8065414d817f96e2b5d9a3f8d493f70b64
James Geddes <jgeddes@jamesgeddes.org>
2026-01-27 20:05:01 +0000

    Working on some code

--- code/p0/prop.rkt
+++ code/p0/prop.rkt
@@ -7,11 +7,11 @@
 A propositional formula is:
 
 Φ ::= #t | #f
-| <symbol?>
-| (¬ Φ)
-| (∧ Φ Ψ)
-| (∨ φ Ψ)
-| (→ Φ Ψ)
+    | <symbol?>        ; atomic proposition
+    | (¬ Φ)
+    | (∧ Φ Ψ)
+    | (∨ φ Ψ)
+    | (→ Φ Ψ)
 
 |#
 
@@ -23,13 +23,36 @@ A propositional formula is:
          (list/c '¬ Φ)
          (list/c (or/c '∧ '∨ '→) Φ Φ))))
 
-;; Compile the set of all identifiers used in a propositional formula
+;; Compile the set of all atomic propositions used in a propositional formula
 ;; prop/c -> set?
-(define (prop-identifiers φ)
+(define (prop-atomics φ)
   (match φ
     [(or #t #f)    '()]
     [(? symbol? v) (list v)]
-    [(list '¬ Φ)   (prop-identifiers Φ)]
+    [(list '¬ Φ)   (prop-atomics Φ)]
     [(list (or '∧ '∨ '→) Φ Ψ)
-                   (set-union (prop-identifiers Φ) (prop-identifiers Ψ))]))
+                   (set-union (prop-atomics Φ) (prop-atomics Ψ))]))
+
+
+;; Flatten a propositional formula to infix notation
+;; optionally removing parentheses
+(define (prop-display φ #:brackets? [brackets? #t])
+  
+
+  )
+;; op : which operator is the ancestor of this sub-formula?
+;; side : 'left or 'right -- which operandum is this?
+(define (subformula->string φ op side brackets?)
+  (match φ
+    [#t            "#t"]
+    [#f            "#f"]
+    [(? symbol? v) (symbol->string v)]
+    [(list '¬ Φ)   (string-join "¬" (subformula->string ψ "" 'right brackets?))]
+    [(list (or '∧ '∨ '→) Φ Ψ)
+                   (set-union (prop-atomics Φ) (prop-atomics Ψ))]
+    )
+  )
+
+;; Exercises from ∀x
+;;
 
