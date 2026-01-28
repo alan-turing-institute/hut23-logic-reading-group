e9da4ab9b916547316b5fb9c7adc9a155f1261af
James Geddes <geddes.james@me.com>
2026-01-28 07:34:17 +0000

    Code

--- code/p0/prop.rkt
+++ code/p0/prop.rkt
@@ -29,30 +29,37 @@ A propositional formula is:
   (match φ
     [(or #t #f)    '()]
     [(? symbol? v) (list v)]
-    [(list '¬ Φ)   (prop-atomics Φ)]
-    [(list (or '∧ '∨ '→) Φ Ψ)
-                   (set-union (prop-atomics Φ) (prop-atomics Ψ))]))
-
+    [(list '¬ ψ)   (prop-atomics ψ)]
+    [(list (or '∧ '∨ '→) ψ χ)
+     (set-union (prop-atomics ψ) (prop-atomics χ))]))
 
 ;; Flatten a propositional formula to infix notation
 ;; optionally removing parentheses
 (define (prop-display φ #:brackets? [brackets? #t])
-  
+  (subformula->string φ 'top 'right brackets?))
 
-  )
 ;; op : which operator is the ancestor of this sub-formula?
 ;; side : 'left or 'right -- which operandum is this?
 (define (subformula->string φ op side brackets?)
+  (define (left α op) (subformula->string α op 'left brackets?))
+  (define (right α op) (subformula->string α op 'right brackets?))
   (match φ
     [#t            "#t"]
     [#f            "#f"]
-    [(? symbol? v) (symbol->string v)]
-    [(list '¬ Φ)   (string-join "¬" (subformula->string ψ "" 'right brackets?))]
-    [(list (or '∧ '∨ '→) Φ Ψ)
-                   (set-union (prop-atomics Φ) (prop-atomics Ψ))]
-    )
-  )
+    [(? symbol? v)
+     (symbol->string v)]
+    [(list '¬ ψ)
+     (string-append "¬" (right ψ '¬))]
+    [(list '∧ ψ χ)
+     (string-append (left ψ '∧) " ∧ " (right χ '∧))]
+    [(list '∨ ψ χ)
+     (string-append (left ψ '∨) " ∨ " (right χ '∨))]
+    [(list '→ ψ χ)
+     (string-append (left ψ '→) " → " (right χ '→))]))
 
 ;; Exercises from ∀x
 ;;
-
+;; 5.D.8 ​If John Coltrane played tuba then Miles Davis played neither trumpet nor tuba. 
+;; J₃ -> ¬M₁ ∧ ¬M₂
+(define/contract D8 prop/c
+  '(→ J₃ (∧ (¬ M₁) (¬ M₂))))
