291d96256a7cb55558acf7b2e1c748366d855774
James Geddes <jgeddes@jamesgeddes.org>
2026-02-01 20:11:56 +0000

    Homework as code

--- code/p0/prop.rkt
+++ code/p0/prop.rkt
@@ -63,14 +63,14 @@ A propositional formula is:
     [(NEG ψ)          (string-append "¬" (subformula->string ψ))]
     [(CONJ ψ χ)
      (string-append
-      (bracket-unless (or/c NEG? CONJ?) ψ)
+      (bracket-unless (or/c ATOM? NEG? CONJ?) ψ)
       " ∧ "
-      (bracket-unless (or/c NEG? CONJ?) χ))]
+      (bracket-unless (or/c ATOM? NEG? CONJ?) χ))]
     [(DISJ ψ χ)
      (string-append
-      (bracket-unless (or/c NEG? DISJ?) ψ)
-      " ∧ "
-      (bracket-unless (or/c NEG? DISJ?) χ))]
+      (bracket-unless (or/c ATOM? NEG? DISJ?) ψ)
+      " ∨ "
+      (bracket-unless (or/c ATOM? NEG? DISJ?) χ))]
     [(IMPL ψ χ)
      (string-append
       (bracket-unless (not/c IMPL?) ψ)
@@ -111,37 +111,64 @@ A propositional formula is:
       (set-union (prop-letters ψ) (prop-letters χ))])
    symbol<?))
 
+;; Show a truth table
+(define (tt φ)
+ (let* ([vars (prop-letters φ)]
+        [rows  (apply cartesian-product
+                      (map (λ (_) '(#f #t)) vars))])
+   (for/list ([vals (in-list rows)])
+     (let* ([env (map cons vars vals)]
+            [val (prop-eval φ env)])
+       (cons vals val))))) 
+
+
 
 (define (make-col-widths vars)
   (map (λ (var)
          (+ 1
-            (max 2 (string-length (symbol->string var)))))))
+            (max 2 (string-length (symbol->string var)))))
+       vars))
 
-(define (format-minimum-width v wd)
+(define (format-all/minimum-widths vs wds)
+  (string-join
+   (map format/minimum-width vs wds)))
+
+(define (format/minimum-width v wd)
   (let ([padding (make-string (- wd (string-length v)) #\space)])
     (string-append padding v)))
 
 ;; -> string
 (define (tt-header vars col-widths φ-string φ-width)
-  (string-append 
-   (string-join (map format-minimum-width (map symbol->string vars) col-widths))
-   " | "
-   (format-minimum-width φ-string φ-width)))
+  (cons
+   (format-all/minimum-widths (map symbol->string vars) col-widths)
+   (format/minimum-width φ-string φ-width)))
 
-(define (truth-table-display φ)
+(define (tt-display φ)
   (let* ([vars       (prop-letters φ)]
          [φ-string   (prop-as-string φ)]
-         [col-widths (make-col-widths (cdr vars))]
+         [col-widths (make-col-widths vars)]
          [φ-width (+ 1 (max 2 (string-length φ-string)))])
-    
-    ;; [rows 
-    ;;  (apply cartesian-product
-    ;;         (map (λ (_) '(#f #t)) vars))]
-    ;; (for ([vals (in-list rows)])
-    ;;   (let ([env (map cons vars vals)])
-    ;;     (let ([val (prop-eval φ env)])
-    ;;       (displayln (format "~a : ~a" vals val)))))))
-    ))
+    (let* ([header (tt-header vars col-widths φ-string φ-width)]
+           [var-header (car header)]
+           [val-header (cdr header)]
+           [var-header-len (string-length var-header)]
+           [val-header-len (string-length val-header)])
+          (displayln (string-append var-header " |" val-header ))
+      (displayln
+       (string-append
+        (make-string var-header-len #\-)
+        "-|"
+        (make-string val-header-len #\-))))
+    (for ([row (in-list (tt φ))])
+      (displayln
+       (string-append
+        (format-all/minimum-widths
+         (map (λ (v) (format "~a" v)) (car row))
+         col-widths)
+        " |"
+        (format/minimum-width
+         (format "~a" (cdr row))
+         φ-width))))))
 
 
 ;; --------------------------------------------------------------------------------
@@ -152,7 +179,26 @@ A propositional formula is:
   (define exA2
     (proposition '(-> C (not C))))
 
-  (prop-as-string exA2)
+  (displayln "11.A.2")
+  (tt-display exA2)
 
+  (displayln "\n\n11.A.5")
+  (tt-display (proposition '(-> (and A B) (or B A))))
 
+(displayln "\n\n11.B.1")
+(tt-display (proposition '(and (and A B) C)))
+(displayln "and")
+(tt-display (proposition '(and A (and B C))))
+
+(displayln "\n\n11.C.3")
+(tt-display
+ (proposition
+  '(and
+    (-> (-> A B)
+        (and (-> (not B) (not A))
+             (-> (not A) (not B))))
+    (-> (and (-> (not B) (not A))
+             (-> (not A) (not B)))
+        (-> A B)))))
+ 
   )
