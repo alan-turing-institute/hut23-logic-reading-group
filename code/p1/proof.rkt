b27489e46a2a2668cd2c663b5721666cb325855d
James Geddes <geddes.james@me.com>
2026-03-16 10:56:25 +0000

    Some code

---
+++ code/p1/proof.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+;; Propositions
+(struct PROP       ()    #:transparent)
+(struct FALSE PROP ()    #:transparent)
+(struct CONJ  PROP (p q) #:transparent)
+(struct DISJ  PROP (p q) #:transparent)
+(struct IMPL  PROP (p q) #:transparent)
+
+
+;; Primitive rules
+(struct AndI   (tl tr)   #:transparent)
+(struct AndE-l (t)       #:transparent)
+(struct AndE-r (t)       #:transparent)
+(struct OrI-l  (t)       #:transparent)
+(struct OrI-r  (t)       #:transparent)
+(struct OrE    (t tl tr) #:transparent)
+(struct App    (t1 t2)   #:transparent)
+
+
+(struct Lam () #:transparent)
+(struct Let () #:transparent)
+
+;; A proof is either:
+;; - a symbol (representing a name)
+;; - a deduction
+;; 
